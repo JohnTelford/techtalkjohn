676d85ce248ce3c99d7be513e004443b4f0a2eb3
John <john@johntelford.com>
2022-12-09 16:35:24 -0800

    initial commit
    Signed-off-by: John <john@johntelford.com>

---
+++ docusaurus.config.js
@@ -0,0 +1,77 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Tech Talk John Blog',
+  tagline: 'Talking Tech',
+  url: 'https://techtalkjohn.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'JohnTelford.com LLC', // Usually your GitHub org/user name.
+  projectName: 'blog', // techtalkjohn.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+
+        
+        },
+        blog: {
+          /* other blog options */
+          blogTitle: 'John Telford Blog',
+          blogDescription: 'Tech Talk John Blog',
+          routeBasePath: '/', // Serve the blog at the site's root
+          showReadingTime: true,
+          blogSidebarTitle: 'Posts',
+          blogSidebarCount: 'ALL',
+          blogListComponent: '@theme/BlogListPage',
+          blogPostComponent: '@theme/BlogPostPage',
+          blogTagsListComponent: '@theme/BlogTagsListPage',
+          blogTagsPostsComponent: '@theme/BlogTagsPostsPage',
+          tagsBasePath: '/blog/tags',
+          archiveBasePath: '/archive',
+        },
+
+      
+
+          theme: {
+            customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Tech Talk John Blog',
+        items: [
+          {to: '/blog/tags', label: 'Topics', position: 'left'},
+        ],
+      },
+      footer: {
+        style: 'dark',
+
+        copyright: `Copyright © ${new Date().getFullYear()} by John M. Telford.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+
+  
+
+};
+
+module.exports = config;
